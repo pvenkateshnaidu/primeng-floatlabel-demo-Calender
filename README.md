b2fd688263122f216e587ec5b6de1090c40dc69e
pvenkateshnaidu <pvenkateshnaidu@gmail.com>
2022-05-03 17:36:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-floatlabel-demo-neft2q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-floatlabel-demo-neft2q)
---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+import { ChipsModule } from 'primeng/chips';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { TableModule } from 'primeng/table';
+
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    FormsModule,
+    HttpClientModule,
+    CalendarModule,
+    ChipsModule,
+    DropdownModule,
+    InputMaskModule,
+    InputNumberModule,
+    CascadeSelectModule,
+    MultiSelectModule,
+    InputTextareaModule,
+    InputTextModule,
+    DialogModule,
+    CheckboxModule,
+    RadioButtonModule,
+    ButtonModule,
+    DropdownModule,
+    TableModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
